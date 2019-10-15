6fc6ab094d820566f2868326625f56289782413a
Noah_Shan <shanwzh@inspur.com>
2019-10-15 17:30:14 +0800

    版本:
    init
    
    更新内容：
    
    Author: Noah_shan

---
+++ rebuild_flutter/lib/USL/testindicate.dart
@@ -0,0 +1,70 @@
+import "dart:convert";
+import "package:flutter/material.dart";
+import "package:http/http.dart" as http;
+import 'package:rebuild_flutter/USL/fromtable.dart';
+
+/// 网络数据加载 - 有指示器的页面
+class Testindicate extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return TestindicateState();
+  }
+}
+
+class TestindicateState extends State<Testindicate> {
+
+  List widgets = [];
+
+  @override 
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  Widget showWhich() {
+    if (widgets.length == 0) { return buildIndicate(); }
+    return buildListVw();
+  }
+
+  @override
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('net data'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.list),onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+              return IITab();
+            }));
+          },)
+        ],
+        ),
+      body: showWhich(),
+    );
+  }
+
+  /// list vw
+  Widget buildListVw() {
+    return ListView.builder(
+      itemCount: widgets.length,
+      itemBuilder: (context, i) {
+        return Padding(padding: EdgeInsets.all(12), child: Text(widgets[i]["title"]));
+      },
+    );
+  }
+
+  /// 指示器
+  Widget buildIndicate() {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  void loadData() async {
+    String strInfo = "https://jsonplaceholder.typicode.com/posts";
+    http.Response response = await http.get(strInfo);
+    setState(() {
+      widgets = json.decode(response.body);
+    });
+  }
+}
---
+++ rebuild_flutter/lib/main.dart
@@ -0,0 +1,156 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'USL/testindicate.dart';
+import 'package:rebuild_flutter/USL/fromtable.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp(
+        title: "halo,world",
+        theme: ThemeData(
+          primaryColor: Colors.white
+        ),
+        home: IITabbar(),//Stsate(),
+      );
+  }
+}
+
+class IITabbar extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return IITabbarState();
+  }
+  
+}
+
+class IITabbarState extends State<IITabbar> {
+  List<Widget> infos = [Testindicate(), IITab(), Stsate()];
+  Widget currentWig = Testindicate();
+  int currentIdx = 0;
+  @override 
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: currentWig,
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            title: Text('首页'),
+            icon: Icon(Icons.home),
+            activeIcon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            title: Text('应用'),
+            icon: Icon(Icons.apps),
+            activeIcon: Icon(Icons.apps),
+          ),
+          BottomNavigationBarItem(
+            title: Text('我的'),
+            activeIcon: Icon(Icons.person),
+            icon: Icon(Icons.person),
+          ),
+        ],
+        selectedItemColor: Colors.teal,
+        currentIndex: currentIdx,
+        onTap: (idx) {
+          setState(() {
+            getWidget(idx);
+          });
+        },
+      ),
+    );
+  }
+
+  void getWidget(int idx) {
+    currentWig = infos[idx];
+    currentIdx = idx;
+  }
+}
+
+class Stsate extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+          appBar: AppBar(
+            title: Text('商品列表'),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.list),
+                onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) {
+                    return Testindicate();
+                  }));
+                },
+              )
+          ],
+          ),
+          body: Center(
+            child: RondamWord()
+          ),
+        );
+  }
+
+}
+
+class RondamWord extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return RondamWordState();
+  }
+}
+
+class RondamWordState extends State<RondamWord> {
+  List<WordPair> _wordsPair = <WordPair>[];
+
+  Set<WordPair> _savedPair = Set<WordPair>();
+  
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.all(15),
+      itemBuilder: (context, i){
+        if (i.isOdd) { return Divider(); }
+        var idx = i ~/ 2;
+        if (idx >= _wordsPair.length) {
+          var generateWords2 = generateWords();
+          _wordsPair.addAll(generateWords2);
+        }
+        return generateRow(_wordsPair[idx]);
+      },
+    );
+  }
+
+  Widget generateRow(WordPair pair) {
+    var _isHaveIt = _savedPair.contains(pair);
+    return ListTile(
+      title: Text(pair.asPascalCase),
+      trailing: Icon(
+        _isHaveIt ? Icons.favorite : Icons.favorite_border,
+        color: _isHaveIt ? Colors.red : null,
+      ),
+      onTap: (){
+        setState(() {
+          _isHaveIt ? _savedPair.remove(pair) : _savedPair.add(pair);
+        });
+      },
+    );
+  }
+
+  List<WordPair> generateWords() {
+    List<WordPair> list = <WordPair>[];
+    for (var i = 0 ; i < 10 ; i++) {
+      list.add(WordPair.random());
+    }
+
+    return list;
+  }
+
+}
