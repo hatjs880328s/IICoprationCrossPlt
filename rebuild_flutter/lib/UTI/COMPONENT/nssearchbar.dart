60599089949448bb3b1be33e9adb0c7fc4f9af1e
Noah_Shan <shanwzh@inspur.com>
2019-10-18 14:01:44 +0800

    版本:
    更新内容：
    
    Author: Noah_shan

--- rebuild_flutter/lib/UTI/COMPONENT/nssearchbar.dart
+++ rebuild_flutter/lib/UTI/COMPONENT/nssearchbar.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
+const searchList = [
+  'jiejie-大长腿',
+  'jiejie-水蛇腰',
+  'gege-帅气欧巴',
+  'gege-小鲜肉'
+];
+
+const recentSuggest = [
+  '推荐-1',
+  '推荐-2',
+];
+
 class nssearchbarDelegate extends SearchDelegate<String> {
 
   @override
@@ -7,7 +19,7 @@ class nssearchbarDelegate extends SearchDelegate<String> {
     return [IconButton(
       icon: Icon(Icons.clear),
       onPressed: () {
-        query == "";
+        query = "";
         buildSuggestions(context);
       },
     )];
@@ -20,12 +32,41 @@ class nssearchbarDelegate extends SearchDelegate<String> {
 
   @override
   Widget buildResults(BuildContext context) {
-    return Text('halo');
+    return Container(
+      width: 100.0,
+      height:100.0,
+      child: Card(
+        color:Colors.redAccent,
+        child: Center(
+          child: Text(query),
+        ),
+      ),
+    );
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return Text('输入文件名');
+    final suggestionsList= query.isEmpty
+      ? recentSuggest 
+      : searchList.where((input)=> input.startsWith(query)).toList();
+
+    return ListView.builder(
+      itemCount:suggestionsList.length,
+      itemBuilder: (context,index) => ListTile(
+        title: RichText( //富文本
+          text: TextSpan(
+            text: suggestionsList[index].substring(0,query.length),
+            style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),
+            children: [
+              TextSpan(
+                text: suggestionsList[index].substring(query.length),
+                style: TextStyle(color: Colors.grey)
+              )
+            ]
+          ),
+        ),
+      ),
+    ); 
   }
 
 }
