cc1d0754d04a7db14484e6f4ad0e9fe3ee99aa37
Noah_Shan <shanwzh@inspur.com>
2019-10-18 19:23:47 +0800

    版本:
    更新内容：
    
    Author: Noah_shan

---
+++ rebuild_flutter/lib/DAL/newlist/newlistlocaldal.dart
@@ -0,0 +1,26 @@
+import 'package:sqflite/sqflite.dart';
+
+class NewListLocalDAL {
+
+  /*
+   * 创建文件数据表 
+   */
+  Future<void> createNewListTab() async {
+    // 获取数据库文件的存储路径
+    // var databasesPath = await getDatabasesPath();
+    // String path = databasesPath.//join(databasesPath, 'demo.db');
+
+//根据数据库文件路径和数据库版本号创建数据库表
+    // var db = await openDatabase(path, version: 1,
+    //     onCreate: (Database db, int version) async {
+    //   await db.execute('''
+    //       CREATE TABLE $tableBook (
+    //         $columnId INTEGER PRIMARY KEY, 
+    //         $columnName TEXT, 
+    //         $columnAuthor TEXT, 
+    //         $columnPrice REAL, 
+    //         $columnPublishingHouse TEXT)
+    //       ''');
+    // });
+  }
+}
