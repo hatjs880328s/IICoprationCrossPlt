37bffc357cfc6e85a0ffbfbccc01dffd340baddc
Noah_Shan <shanwzh@inspur.com>
2019-10-21 10:48:38 +0800

    版本:
    更新内容：
    
    Author: Noah_shan

--- rebuild_flutter/lib/USL/TabbarGroup/maintabbar.dart
+++ rebuild_flutter/lib/USL/TabbarGroup/maintabbar.dart
@@ -6,6 +6,7 @@ import 'package:rebuild_flutter/USL/MAIN/main.dart';
 import 'package:rebuild_flutter/USL/NewFile/newestfile.dart';
 
 class MaintabBar extends StatefulWidget {
+  List<Widget> infos = [NewestFile(), IITab(), Stsate(), Stsate()];
   @override
   State<StatefulWidget> createState() {
     return MaintabBarState();
@@ -13,30 +14,38 @@ class MaintabBar extends StatefulWidget {
 }
 
 class MaintabBarState extends State<MaintabBar> {
-  List<Widget> infos = [NewestFile(), IITab(), Stsate()];
+  
   Widget currentItem = NewestFile();
   int currentIdx = 0;
   Widget build(BuildContext context) {
     return Scaffold(
       body: currentItem,
-      bottomNavigationBar: CupertinoTabBar(
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('首页'), activeIcon: Icon(Icons.home)),
-          BottomNavigationBarItem(icon: Icon(Icons.apps), title: Text('应用'), activeIcon: Icon(Icons.apps)),
-          BottomNavigationBarItem(icon: Icon(Icons.person), title: Text('我的'), activeIcon: Icon(Icons.person))
-        ],
-        currentIndex: currentIdx,
-        onTap: (idx) {
-          this.selectItem(idx);
-        },
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 4,
+        child: Row(
+          children: <Widget>[
+            IconButton(icon: Icon(Icons.home), onPressed: () { this.selectItem(0); }),
+            IconButton(icon: Icon(Icons.chat), onPressed: () {this.selectItem(1);}),
+            SizedBox(),
+            IconButton(icon: Icon(Icons.apps), onPressed: () {this.selectItem(2);}),
+            IconButton(icon: Icon(Icons.person), onPressed: () {this.selectItem(3);})
+          ],
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+        ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 
   void selectItem(int idx) {
     setState(() {
       currentIdx = idx;
-      currentItem = infos[idx];
+      currentItem = widget.infos[idx];
     });
     
   }
