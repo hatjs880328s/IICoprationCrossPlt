8342979e52c074951dd373d7b130fd76cfcf38f6
Noah_Shan <shanwzh@inspur.com>
2019-10-24 17:05:59 +0800

    版本:
    更新内容：
    
    Author: Noah_shan

--- rebuild_flutter/lib/USL/Login/loginpage.dart
+++ rebuild_flutter/lib/USL/Login/loginpage.dart
@@ -34,7 +34,7 @@ class LoginPageState extends State<LoginPage> {
               height: 80,
               width: 80,
               margin:
-                  EdgeInsets.fromLTRB(25, NSNormalConfig.appAppBarHeight, 0, 0),
+                  EdgeInsets.fromLTRB(25, NSNormalConfig.appAppBarHeight + 20, 0, 0),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(40),
                 image: DecorationImage(
@@ -45,6 +45,7 @@ class LoginPageState extends State<LoginPage> {
             ),
           ],
         ),
+        /// 欢迎文案
         Container(
           alignment: Alignment.topLeft,
           margin: EdgeInsets.fromLTRB(25, 15, 0, 0),
@@ -53,6 +54,84 @@ class LoginPageState extends State<LoginPage> {
             style:
                 TextStyle(fontFamily: NSNormalConfig.fontFamily, fontSize: 28),
             textAlign: TextAlign.left,
+          ),
+        ),
+        /// 邮箱提示
+        Container(
+          alignment: Alignment.topLeft,
+          margin: EdgeInsets.only(top: 30, left: 25),
+          child: Text('邮箱账号', style: TextStyle(fontFamily: NSNormalConfig.fontFamily, fontSize: 13), textAlign: TextAlign.left,),
+        ),
+        /// 邮箱输入框
+        Container(
+          margin: EdgeInsets.only(left: 25, right: 25, top: 10),
+          child: TextField(
+            style: TextStyle(fontSize: 16, fontFamily: NSNormalConfig.fontFamily),
+            decoration: InputDecoration(
+              hintText: "请输入邮箱账号",
+              suffixIcon: IconButton(
+                icon: Icon(Icons.send),
+                onPressed: () {
+
+                },
+              ),
+            ),
+          ),
+        ),
+        /// 验证码提示
+        Container(
+          alignment: Alignment.topLeft,
+          margin: EdgeInsets.only(top: 30, left: 25),
+          child: Text('验证码', style: TextStyle(fontFamily: NSNormalConfig.fontFamily, fontSize: 13), textAlign: TextAlign.left,),
+        ),
+        /// 验证码
+        Container(
+          margin: EdgeInsets.only(left: 25, right: 25, top: 10),
+          child: TextField(
+            style: TextStyle(fontSize: 16, fontFamily: NSNormalConfig.fontFamily),
+            decoration: InputDecoration(
+              hintText: "请输入验证码"
+            ),
+          ),
+        ),
+        /// 登陆按钮
+        Container(
+          margin: EdgeInsets.only(left: 25, right: 25, top: 45),
+          height: 40,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            color: Colors.grey,
+            borderRadius: BorderRadius.circular(3)
+          ),
+          child: GestureDetector(
+            onTap: () {},
+            child: Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.only(top: 2),
+              child: Text('登陆', style: TextStyle(fontFamily: NSNormalConfig.fontFamily, fontSize: 20),),
+            )
+          ),
+        ),
+        /// 第三方登陆
+        Container(
+          margin: EdgeInsets.only(left: 25, right: 25, top: 30),
+          child: Row(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('asdfasdf'),
+              //微信
+              // Image(
+              //   image: AssetImage('images/login_weixin.png'),
+              // ),
+              // //qq
+              // Image(
+              //   image: AssetImage('images/login_qq.png'),
+              // ),
+              // //新浪微博
+              // Image(
+              //   image: AssetImage('images/login_weibo.png'),
+              // )
+            ],
           ),
         ),
       ],
