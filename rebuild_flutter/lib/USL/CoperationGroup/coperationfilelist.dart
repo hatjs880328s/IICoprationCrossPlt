5a2937068cfd5f4ae9a411a5de36ca6cb6212fb1
Noah_Shan <shanwzh@inspur.com>
2019-11-05 09:06:29 +0800

    版本:
    更新内容：
    
    Author: Noah_shan

--- rebuild_flutter/lib/USL/CoperationGroup/coperationfilelist.dart
+++ rebuild_flutter/lib/USL/CoperationGroup/coperationfilelist.dart
@@ -2,14 +2,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:rebuild_flutter/BLL/CoperationGroupBLL/coperationgroupbll.dart';
-import 'package:rebuild_flutter/MODEL/Newfile/foldermodel.dart';
 import 'package:rebuild_flutter/USL/CoperationGroup/coperationfilelistfirstcell.dart';
 import 'package:rebuild_flutter/USL/CoperationGroup/coperationfilelistforthcell.dart';
 import 'package:rebuild_flutter/USL/CoperationGroup/coperationfilelistsecondcell.dart';
 import 'package:rebuild_flutter/USL/CoperationGroup/coperationfilelistthirdcell.dart';
-import 'package:rebuild_flutter/USL/CoperationGroup/coperitiongroupcell.dart';
-import 'package:rebuild_flutter/UTI/COMPONENT/IIAnimationColor/iianimationcolor.dart';
-import 'package:rebuild_flutter/UTI/COMPONENT/NSActionSheet/nsactionfield.dart';
 
 class CoperationFileList extends StatefulWidget {
   @override
@@ -27,10 +23,13 @@ class CoperationFileList extends StatefulWidget {
 class CoperationFileListState extends State<CoperationFileList> {
   List<String> list = [];
 
+  ScrollController scrollCon;
+
   @override
   void initState() {
     super.initState();
     this.getGroupinfo();
+    this.scrollCon = ScrollController();
   }
 
   @override
@@ -41,32 +40,34 @@ class CoperationFileListState extends State<CoperationFileList> {
       child: Stack(
         children: <Widget>[
           Positioned(
-            top: 0, 
-            left: 0, 
-            right: 0,
-            height: 220,
-            child: Container(
-              decoration: BoxDecoration(border: Border.all(width: 0.5, color: Colors.grey)),
-              child: Image(image: AssetImage('images/cop_128.png'), fit: BoxFit.fill)
-            )
-          ),
+              top: 0,
+              left: 0,
+              right: 0,
+              height: 220,
+              child: Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(width: 0.5, color: Colors.grey)),
+                  child: Image(
+                      image: AssetImage('images/cop_128.png'),
+                      fit: BoxFit.fill))),
           Positioned(
-            left: 0,
-            right: 0,
-            bottom: 0,
-            top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              top: 0,
               child: ListView(
-            children: <Widget>[
-              //第一部分（返回按钮 & 顶部分享、邀请等按钮）
-              CoperationFileListFirstCell(),
-              //第二部分协同组基本信息
-              CoperationFileListSecondCell(),
-              //第三部分协同成员信息
-              CoperationFileListCell(),
-              //第四部分协同组文章
-              CoperationFileListForthCell(),
-            ],
-          )),
+                controller: this.scrollCon,
+                children: <Widget>[
+                  //第一部分（返回按钮 & 顶部分享、邀请等按钮）
+                  CoperationFileListFirstCell(),
+                  //第二部分协同组基本信息
+                  CoperationFileListSecondCell(),
+                  //第三部分协同成员信息
+                  CoperationFileListCell(),
+                  //第四部分协同组文章
+                  CoperationFileListForthCell(),
+                ],
+              )),
         ],
       ),
     ));
