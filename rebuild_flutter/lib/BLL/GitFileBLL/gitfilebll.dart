d743c20919f7671b22dae833ca6213224896a63a
Noah_Shan <shanwzh@inspur.com>
2019-11-13 15:09:40 +0800

    版本:
    更新内容：
    
    Author: Noah_shan

--- rebuild_flutter/lib/BLL/GitFileBLL/gitfilebll.dart
+++ rebuild_flutter/lib/BLL/GitFileBLL/gitfilebll.dart
@@ -1,4 +1,3 @@
-
 import 'dart:convert';
 import 'package:convert/convert.dart';
 import 'package:fluttertoast/fluttertoast.dart';
@@ -14,7 +13,6 @@ import 'package:uuid/uuid.dart';
 import 'package:crypto/crypto.dart';
 
 class GitFileBLL {
-
   /// db - dal
   NewListLocalDAL localDal = NewListLocalDAL();
 
@@ -25,40 +23,43 @@ class GitFileBLL {
    * 2.创建内容时，是一个model,tojson.tostring()
    * 3.如果folderorgroupname为null,则是创建一个普通文件，需要放到最新文件夹下面；否则就是一个协同文档
    */
-  Future createFile(
-    CoperationGroupModel oldModel,
-    String content, 
-      String folderorgroupname,
-      String filename, {String subTitle}) async {
-        var folderorgroupnames = (folderorgroupname == null) ? "最新" : folderorgroupname;
-        var iffolder = (folderorgroupname == null) ? true : false;
-        var model = await NSLoginGlobal.getInstance().getUserInfo();
-        String uid = model.uid;
-        NewListDAL dal = NewListDAL();
-        String title = filename + "EXEOF" + DateTime.now().millisecondsSinceEpoch.toString();
-        RealGitFileModel realmodel = RealGitFileModel(
-          new Uuid().v1(),
-          content, 
-          DateTime.now().millisecondsSinceEpoch.toDouble(), 
-          "", 
-          title, 
-          subTitle == null ? content.substring(0, content.length > 10 ? 10 : content.length - 1) : subTitle
-          );
-        await dal.createFile(json.encode(realmodel.toJson()), uid, folderorgroupnames, iffolder, title);
+  Future createFile(CoperationGroupModel oldModel, String content,
+      String folderorgroupname, String filename,
+      {String subTitle}) async {
+    var folderorgroupnames =
+        (folderorgroupname == null) ? "最新" : folderorgroupname;
+    var iffolder = (folderorgroupname == null) ? true : false;
+    var model = await NSLoginGlobal.getInstance().getUserInfo();
+    String uid = model.uid;
+    NewListDAL dal = NewListDAL();
+    String title =
+        filename + "EXEOF" + DateTime.now().millisecondsSinceEpoch.toString();
+    RealGitFileModel realmodel = RealGitFileModel(
+        new Uuid().v1(),
+        content,
+        DateTime.now().millisecondsSinceEpoch.toDouble(),
+        "",
+        title,
+        subTitle == null
+            ? content.substring(
+                0, content.length > 10 ? 10 : content.length - 1)
+            : subTitle);
+    await dal.createFile(json.encode(realmodel.toJson()), uid,
+        folderorgroupnames, iffolder, title);
 
-        if (oldModel != null) {
-          /// 如果不为空 - 新建一个协同组文章的时候，需要更新协同组info文件; 记得将model的具体内容干掉
-          CoperationGroupBLL groupbll = CoperationGroupBLL();
-          realmodel.content = "";
-          oldModel.files.add(realmodel);
-          String realpath = "/${model.uid}/Groups/${oldModel.name}/GroupInfoFile";
-          FolderModel sha = await this.getOneFileSha(realpath);
-          groupbll.updateGroupInfo(oldModel.name, oldModel, sha.sha);
-        }
-        Fluttertoast.showToast(
-          msg: "创建成功",
-          gravity: ToastGravity.CENTER,
-        );
+    if (oldModel != null) {
+      /// 如果不为空 - 新建一个协同组文章的时候，需要更新协同组info文件; 记得将model的具体内容干掉
+      CoperationGroupBLL groupbll = CoperationGroupBLL();
+      realmodel.content = "";
+      oldModel.files.add(realmodel);
+      String realpath = "/${model.uid}/Groups/${oldModel.name}/GroupInfoFile";
+      FolderModel sha = await this.getOneFileSha(realpath);
+      groupbll.updateGroupInfo(oldModel.name, oldModel, sha.sha);
+    }
+    Fluttertoast.showToast(
+      msg: "创建成功",
+      gravity: ToastGravity.CENTER,
+    );
   }
 
   /*
@@ -68,7 +69,7 @@ class GitFileBLL {
     FolderDAL dal = FolderDAL();
     Map item = await dal.getOneItem(path);
     FolderModel newmodel = FolderModel.fromJson(item);
-    
+
     return newmodel;
   }
 
@@ -88,13 +89,15 @@ class GitFileBLL {
     List<Map> result = await this.localDal.getInfo(wheresql);
     if (result.length > 0) {
       RealGitFileModel model = RealGitFileModel.fromJson(result.first);
-      if (model != null) { return model; }
+      if (model != null) {
+        return model;
+      }
     }
     //再从网络取[获取下来的首先是gitmodel -> content -> base64decode -> realgitfilemodel]
     FolderDAL dal = FolderDAL();
     Map item = await dal.getOneItem(path);
     FolderModel newmodel = FolderModel.fromJson(item);
-    
+
     var data = base64Decode(newmodel.content.replaceAll("\n", ""));
     String contents = utf8.decode(data);
     Map infos = json.decode(contents);
