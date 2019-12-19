0bf22c3467282ae722b1f1285701a2f5d8edab11
Noah_Shan <shanwzh@inspur.com>
2019-12-19 10:27:36 +0800

    版本:
    更新内容：
    
    Author: Noah_shan

--- rebuild_flutter/lib/USL/ReduxAPP/empty/main_page/effect.dart
+++ rebuild_flutter/lib/USL/ReduxAPP/empty/main_page/effect.dart
@@ -1,7 +1,6 @@
 import 'package:fish_redux/fish_redux.dart';
 import 'package:flutter/material.dart' hide Action;
 import 'package:flutter/widgets.dart' hide Action;
-import 'package:rebuild_flutter/USL/ReduxAPP/empty/gird_page/page.dart'; 
 import 'action.dart';
 import 'state.dart';
 
